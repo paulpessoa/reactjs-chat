2d43e025ff952b8154ab8803196697d467e47798
Paul Pessoa <paulmspessoa@gmail.com>
2023-07-04 16:48:42 -0300

    secret key backend

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const {default: axios } = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "private-key": "b31a4fe9-ef8e-4683-8f97-7054caa0b8df" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+app.listen(3001);
+
+
+
+
+
+
+
+
+
+
+
